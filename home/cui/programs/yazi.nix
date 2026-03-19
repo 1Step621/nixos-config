e8ba013e97203c67698ac4befc0c195163ad80c4
1Step621 <onestep621@gmail.com>
2026-03-19 17:32:06 +0900

    yazi: style

--- home/cui/programs/yazi.nix
+++ home/cui/programs/yazi.nix
@@ -1,8 +1,46 @@
+{ pkgs, ... }:
 {
   programs.yazi = {
     enable = true;
     enableZshIntegration = true;
     shellWrapperName = "y";
+
+    plugins = with pkgs.yaziPlugins; {
+      inherit full-border;
+    };
+
+    initLua = ''
+      require("full-border"):setup()
+    '';
+
+    theme = {
+      indicator.padding = {
+        open = "▐";
+        close = "▌";
+      };
+      mode = {
+        normal_alt = {
+          fg = "black";
+          bg = "gray";
+        };
+        select_alt = {
+          fg = "black";
+          bg = "gray";
+        };
+      };
+      status = {
+        overall.fg = "black";
+        progress_label.fg = "black";
+        sep_left = {
+          open = "";
+          close = "";
+        };
+        sep_right = {
+          open = "";
+          close = "";
+        };
+      };
+    };
 
     settings = {
       opener.pdf = [
