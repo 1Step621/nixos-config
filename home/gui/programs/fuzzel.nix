54dabbb701cade0eb5ee019f782315a8f2c8a33f
1Step621 <onestep621@gmail.com>
2026-03-20 21:53:43 +0900

    fuzzel: tweak theme

--- home/gui/programs/fuzzel.nix
+++ home/gui/programs/fuzzel.nix
@@ -4,16 +4,23 @@
     settings = {
       main = {
         font = "Inter:size=10";
-        line-height = 15;
+        line-height = 17;
+        image-size-ratio = 1;
+        prompt = "\"\"";
+        inner-pad = 10;
+        lines = 5;
       };
       colors = {
-        background = "282a36dd";
-        text = "f8f8f2ff";
-        match = "8be9fdff";
-        selection-match = "8be9fdff";
-        selection = "44475add";
-        selection-text = "f8f8f2ff";
-        border = "bd93f9ff";
+        background = "282c34dd";
+        text = "abb2bfff";
+        match = "61afefff";
+        selection-match = "61afefff";
+        selection = "3e4451dd";
+        selection-text = "abb2bfff";
+        border = "61afefff";
+      };
+      border = {
+        selection-radius = 8;
       };
       key-bindings = {
         delete-line-forward = "none";
