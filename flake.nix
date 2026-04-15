0ab35c457251920df3e8d9cd11fe6dcb287a30cb
1Step621 <onestep621@gmail.com>
2026-04-16 01:18:01 +0900

    flake.lock: update

--- flake.nix
+++ flake.nix
@@ -7,10 +7,6 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixos-wsl = {
-      url = "github:nix-community/NixOS-WSL";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     nur-yadokani = {
       url = "github:yadokani389/nur-packages";
       inputs = {
