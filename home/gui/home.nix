0e94e5d8bf03fa26d26357981dd0d18024d793f0
1Step621 <onestep621@gmail.com>
2026-04-19 16:34:53 +0900

    xdg: update defaultApplications

--- home/gui/home.nix
+++ home/gui/home.nix
@@ -62,6 +62,25 @@
       "x-scheme-handler/https" = "zen-beta.desktop";
       "x-scheme-handler/about" = "zen-beta.desktop";
       "x-scheme-handler/unknown" = "zen-beta.desktop";
+      "application/pdf" = "org.gnome.Papers.desktop";
+      "text/plain" = "code.desktop";
+      "text/csv" = "code.desktop";
+      "text/x-c++hdr" = "code.desktop";
+      "text/x-c++src" = "code.desktop";
+      "text/x-python" = "code.desktop";
+      "text/x-shellscript" = "code.desktop";
+      "text/x-java" = "code.desktop";
+      "text/x-csharp" = "code.desktop";
+      "text/x-go" = "code.desktop";
+      "text/x-rust" = "code.desktop";
+      "text/x-javascript" = "code.desktop";
+      "text/x-typescript" = "code.desktop";
+      "text/markdown" = "code.desktop";
+      "application/yaml" = "code.desktop";
+      "application/json" = "code.desktop";
+      "application/xml" = "code.desktop";
+      "text/x-cmake" = "code.desktop";
+      "application/toml" = "code.desktop";
     };
   };
 
